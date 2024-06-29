c34001eda5108ea0df3ec5c6ef3a354e14563ef9
Alex Silva <alex.silva8@outlook.com>
2024-06-29 13:45:36 -0300

    Create zerovalues.go
    Entendendo o conceito de zero values

---
+++ estudos/zerovalues.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	//Entendendo o conceito de zero values
+	var i int
+	var f float64
+	var b bool
+	var s string
+	fmt.Printf("Inteiro: %v\n", i)
+	fmt.Printf("Float: %v\n", f)
+	fmt.Printf("Boolean: %v\n", b)
+	fmt.Printf("String: %q\n", s)
+}
